b5c6b38b8cc45f1f5bdff1d05fe22f25b5d5fda0
jsegal <segalsmail@gmail.com>
2025-12-31 14:11:36 -0800

    Updated AuthContext.tsx

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -53,7 +53,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}?authenticated=true`,
+        redirectTo: window.location.origin,
       },
     });
     return { error };
